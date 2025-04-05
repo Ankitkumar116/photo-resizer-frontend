a5d573bbf1848955137492570e779e4bbea5bfc1
Ankit Kumar <pekka0555@gmail.com>
2025-04-05 14:15:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/photo-resizer-frontend/', // <-- This is important!
+  base: '/photo-resizer-frontend/', // important for GitHub Pages!
 });
+
